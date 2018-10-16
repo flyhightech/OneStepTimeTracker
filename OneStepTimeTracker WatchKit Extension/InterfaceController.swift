5f7a5cdfafe57a55cae5868797f871899bb7fb0d
Bernard Huff <bernard.huff008@gmail.com>
2018-10-16 18:01:45 -0500

    Initial Commit

---
+++ OneStepTimeTracker WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  OneStepTimeTracker WatchKit Extension
+//
+//  Created by Bernard Huff on 10/16/18.
+//  Copyright © 2018 Flyhightech.LLC. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
